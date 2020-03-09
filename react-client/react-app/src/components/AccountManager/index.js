8c8cddb1d3e23ce620fd4b222bfba6ad8970d0c5
James Webb <james.webb7@tesco.com>
2020-03-09 23:18:49 +0000

    WIP

--- react-client/react-app/src/components/AccountManager/index.js
+++ react-client/react-app/src/components/AccountManager/index.js
@@ -21,12 +21,34 @@ class AccountManager extends React.Component {
   }
 
   render = () => {
-    return (
-      <div>
-        <div>AccountManager</div>
-        <button onClick={() => window.location.href = LOGIN_URL}>Log in</button>
-      </div>
-    );
+    if (this.state.currentUser.accessToken){
+      return (
+        <div>
+          Would you like to stay logged in?
+          {/* make call to user profile on spotify and find user account */}
+        </div>
+      )
+    }
+    else{
+      return (
+        <div>
+          <div>
+            Here is a list of your accounts
+          </div>
+          <div>
+            Would you like to add a new one?
+            <button onClick={() => window.location.href = LOGIN_URL}>Log in</button>
+          </div>
+        </div>
+      )
+    }
+
+    // return (
+    //   <div>
+    //     <div>AccountManager</div>
+    //     <button onClick={() => window.location.href = LOGIN_URL}>Log in</button>
+    //   </div>
+    // );
   };
 }
 
