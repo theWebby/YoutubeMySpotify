13d2b84d8d094a4ac36d0ba989c4c3d088284cf4
James Webb <jrwebb1@sheffield.ac.uk>
2020-03-31 17:17:24 +0100

    rebuild

---
+++ react-client/react-app/build/precache-manifest.f425fa69178c0226f229105dd5155b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fca100e457fa324550dd1a73e97aa1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf2a7b9e295a3e687440",
+    "url": "./static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "80989f7059a2b7010b95",
+    "url": "./static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "cf2a7b9e295a3e687440",
+    "url": "./static/js/2.deef22e9.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "./static/js/2.deef22e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80989f7059a2b7010b95",
+    "url": "./static/js/main.f007652f.chunk.js"
+  },
+  {
+    "revision": "e550f0e0d0d510cb559a",
+    "url": "./static/js/runtime-main.dff49d37.js"
+  }
+]);
--- react-client/react-app/build/service-worker.js
+++ react-client/react-app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0ff056d7aced26845b91c3fd778b486d.js"
+  "./precache-manifest.f425fa69178c0226f229105dd5155b58.js"
 );
 
 self.addEventListener('message', (event) => {
