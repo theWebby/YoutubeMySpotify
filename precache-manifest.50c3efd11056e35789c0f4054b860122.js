5e4709127e73700722236d6f0d44e2a4f16607b0
James Webb <jrwebb1@sheffield.ac.uk>
2020-04-05 19:31:11 +0100

    Updates

--- precache-manifest.50c3efd11056e35789c0f4054b860122.js
+++ precache-manifest.50c3efd11056e35789c0f4054b860122.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7bffded967538552d1b335d448e14c33",
+    "revision": "8c52297afdbffd3bea0c41be8bc2c45e",
     "url": "/YoutubeMySpotify/index.html"
   },
   {
-    "revision": "a88e3a620190a2a79632",
+    "revision": "f10728b7cd21363d0f89",
     "url": "/YoutubeMySpotify/static/css/2.47e06e2e.chunk.css"
   },
   {
-    "revision": "9f5c1470ba4842c15306",
+    "revision": "11cacf4c81cc9b129a78",
     "url": "/YoutubeMySpotify/static/css/main.69eda509.chunk.css"
   },
   {
-    "revision": "a88e3a620190a2a79632",
-    "url": "/YoutubeMySpotify/static/js/2.3622f752.chunk.js"
+    "revision": "f10728b7cd21363d0f89",
+    "url": "/YoutubeMySpotify/static/js/2.fcdfa914.chunk.js"
   },
   {
     "revision": "299cba5f6c747cbc295097594263041b",
-    "url": "/YoutubeMySpotify/static/js/2.3622f752.chunk.js.LICENSE.txt"
+    "url": "/YoutubeMySpotify/static/js/2.fcdfa914.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9f5c1470ba4842c15306",
-    "url": "/YoutubeMySpotify/static/js/main.43835427.chunk.js"
+    "revision": "11cacf4c81cc9b129a78",
+    "url": "/YoutubeMySpotify/static/js/main.8431e359.chunk.js"
   },
   {
     "revision": "8b6718e1b15d4b69724c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/YoutubeMySpotify/precache-manifest.83b8200504e07caaf59521f0978af492.js"
+  "/YoutubeMySpotify/precache-manifest.50c3efd11056e35789c0f4054b860122.js"
 );
 
 self.addEventListener('message', (event) => {
