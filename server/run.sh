660d99b72953ae437fa6b231651bf75fcfa11bfe
James Webb <jrwebb1@sheffield.ac.uk>
2020-05-25 17:46:08 +0100

    WIP

--- server/run.sh
+++ server/run.sh
@@ -1,4 +1,4 @@
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker build --tag yms-server:1.0 .
-sudo docker run --rm -it -v /etc/letsencrypt/archive/youtubemyspotify.uk:/etc/letsencrypt/archive/youtubemyspotify.uk:ro -p 3000:3000 -p 443:443
+sudo docker run --rm -it -v /etc/letsencrypt/archive/youtubemyspotify.uk:/etc/letsencrypt/archive/youtubemyspotify.uk:ro -p 3000:3000 -p 443:443 yms-server:1.0
 
