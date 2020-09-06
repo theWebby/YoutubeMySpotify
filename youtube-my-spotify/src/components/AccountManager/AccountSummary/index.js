110dc716fc057fd7c4884f71d658ccfc34ff70f0
theWebby <jameswebb15@hotmail.co.uk>
2020-09-06 19:28:54 +0100

    Default user logo

--- youtube-my-spotify/src/components/AccountManager/AccountSummary/index.js
+++ youtube-my-spotify/src/components/AccountManager/AccountSummary/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AccountSummaryContainer, StyledImage as Image, Name, Info, InfoContainer } from "./styled"
 import request from "../../../api/request";
+import defaultUserImgPath from '../../../images/user.png'
 
 class AccountSummary extends React.Component {
   constructor(props) {
@@ -12,9 +13,9 @@ class AccountSummary extends React.Component {
 
     return (
       <AccountSummaryContainer>
-         <iframe style={{display: 'none'}} src="https://spotify.com/logout"></iframe> 
+        <iframe style={{display: 'none'}} src="https://spotify.com/logout"></iframe> 
 
-        <Image src={profile.images.length ? profile.images[0].url : 'https://pmcdeadline2.files.wordpress.com/2019/10/shutterstock_editorial_10434333bm.jpg?crop=0px%2C0px%2C2903px%2C1627px&resize=681%2C383'} roundedCircle />
+        <Image src={profile.images.length ? profile.images[0].url : defaultUserImgPath } roundedCircle />
         <Name>{profile.display_name}</Name>
         <InfoContainer>
           <Info>{profile.followers.total} followers</Info> | <Info>{profile.product === 'premium' ? 'Premium' : 'Free Account'}</Info>
