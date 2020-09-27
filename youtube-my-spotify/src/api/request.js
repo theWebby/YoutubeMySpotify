e9a554b80f65b6784abe1d65d449c6406e0f4546
theWebby <jameswebb15@hotmail.co.uk>
2020-09-27 15:17:53 +0100

    Unknown context breaking app fixed

--- youtube-my-spotify/src/api/request.js
+++ youtube-my-spotify/src/api/request.js
@@ -1,27 +1,27 @@
-export default function request(theUrl, method, accessToken, params = null){
-    return new Promise((resolve, reject) => {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {     
-            if (xmlHttp.readyState === 4 && (xmlHttp.status === 200)){
-                resolve(JSON.parse(xmlHttp.responseText));
-            }
-            if (xmlHttp.readyState === 4 && (xmlHttp.status === 204)){
-                resolve();
-            }
-            if (xmlHttp.readyState === 4){
-                reject(xmlHttp)
-            }
-        }
+export default function request(theUrl, method, accessToken, params = null) {
+  return new Promise((resolve, reject) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+        resolve(JSON.parse(xmlHttp.responseText));
+      }
+      if (xmlHttp.readyState === 4 && xmlHttp.status === 204) {
+        resolve();
+      }
+      if (xmlHttp.readyState === 4) {
+        reject(xmlHttp);
+      }
+    };
 
-        xmlHttp.open(method, theUrl, true); // true for asynchronous 
-        if (accessToken){
-            xmlHttp.setRequestHeader('Authorization', 'Bearer ' + accessToken)
-        }
-        if (params){
-            xmlHttp.setRequestHeader('Content-Type', 'application/json');
-        }
-        xmlHttp.send(JSON.stringify(params));
-    })
+    xmlHttp.open(method, theUrl, true); // true for asynchronous
+    if (accessToken) {
+      xmlHttp.setRequestHeader("Authorization", "Bearer " + accessToken);
+    }
+    if (params) {
+      xmlHttp.setRequestHeader("Content-Type", "application/json");
+    }
+    xmlHttp.send(JSON.stringify(params));
+  });
 
-    //webby sux
+  //webby sux
 }
--- youtube-my-spotify/src/api/spotifyApi.js
+++ youtube-my-spotify/src/api/spotifyApi.js
@@ -1,63 +1,113 @@
-import request from "./request"
-
+import request from "./request";
 
 export default class SpotifyApi {
-    constructor(accessToken, refreshToken){
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
+  constructor(accessToken, refreshToken) {
+    this.accessToken = accessToken;
+    this.refreshToken = refreshToken;
+  }
+
+  async _makeRequest(url, method, accessToken) {
+    try {
+      return await request(url, method, accessToken);
+    } catch (e) {
+      if (e.status === 401) {
+        this.accessToken = (
+          await this.getNewAccessToken(this.refreshToken)
+        ).access_token;
+        return request(url, method, this.accessToken);
+      }
+
+      throw e;
+    }
+  }
+
+  async getPlaylist(playlistUrl) {
+    const recommendedString = ":recommended";
+    let sanitisedPlaylistUrl = playlistUrl;
+    let isRecommended = false;
+
+    if (playlistUrl.includes(recommendedString)) {
+      sanitisedPlaylistUrl = playlistUrl.replace(recommendedString, ""); //Recommended songs has :recommended at the end of the url - probably should deal with this better.
+      isRecommended = true;
     }
 
-    async _makeRequest(url, method, accessToken){
-        try{
-            return await request(url, method, accessToken);
-        }
-        catch (e) {
-            if (e.status === 401){
-                this.accessToken = (await this.getNewAccessToken(this.refreshToken)).access_token;
-                return request(url, method, this.accessToken);
-            }
+    const playlistData = await this.get(sanitisedPlaylistUrl);
+    playlistData.isRecommended = isRecommended;
+    return playlistData;
+  }
 
-            throw e
-        }
-    }
+  get(url) {
+    return this._makeRequest(url, "GET", this.accessToken);
+  }
 
-    get(url){
-        return this._makeRequest(url, 'GET', this.accessToken);
-    }
+  getCurrentlyPlaying() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me/player/currently-playing",
+      "GET",
+      this.accessToken
+    );
+  }
 
-    getCurrentlyPlaying(){
-        return this._makeRequest('https://api.spotify.com/v1/me/player/currently-playing', 'GET', this.accessToken);
-    }
+  play() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me/player/play",
+      "PUT",
+      this.accessToken
+    );
+  }
 
-    play(){
-        return this._makeRequest('https://api.spotify.com/v1/me/player/play', 'PUT', this.accessToken);
-    }
+  pause() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me/player/pause",
+      "PUT",
+      this.accessToken
+    );
+  }
 
-    pause(){
-        return this._makeRequest('https://api.spotify.com/v1/me/player/pause', 'PUT', this.accessToken)
-    }
+  skip() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me/player/next",
+      "POST",
+      this.accessToken
+    );
+  }
 
-    skip(){
-        return this._makeRequest('https://api.spotify.com/v1/me/player/next', 'POST', this.accessToken);
-    }
-    
-    prev(){
-        return this._makeRequest('https://api.spotify.com/v1/me/player/previous', 'POST', this.accessToken);
-    }
+  prev() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me/player/previous",
+      "POST",
+      this.accessToken
+    );
+  }
 
-    seek(ms){
-        return this._makeRequest(`https://api.spotify.com/v1/me/player/seek?position_ms=${ms}`, 'PUT', this.accessToken);
-    }
+  seek(ms) {
+    return this._makeRequest(
+      `https://api.spotify.com/v1/me/player/seek?position_ms=${ms}`,
+      "PUT",
+      this.accessToken
+    );
+  }
 
-    getProfile(){
-        return this._makeRequest('https://api.spotify.com/v1/me', 'GET', this.accessToken)
-    }
+  getProfile() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me",
+      "GET",
+      this.accessToken
+    );
+  }
 
-    getTopTracks(){
-        return this._makeRequest('https://api.spotify.com/v1/me/top/tracks?limit=50', 'GET', this.accessToken)
-    }
+  getTopTracks() {
+    return this._makeRequest(
+      "https://api.spotify.com/v1/me/top/tracks?limit=50",
+      "GET",
+      this.accessToken
+    );
+  }
 
-    getNewAccessToken(refreshToken){
-        return request(`https://youtubemyspotify.uk/refresh_token?refresh_token=${this.refreshToken}`, 'GET');
-    }
+  getNewAccessToken(refreshToken) {
+    return request(
+      `https://youtubemyspotify.uk/refresh_token?refresh_token=${this.refreshToken}`,
+      "GET"
+    );
+  }
 }
